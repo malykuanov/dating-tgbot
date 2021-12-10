5f74ed48382f6cb55f70aa6e57f7f848beb9c17b
Mikhail Lykuanov <malykuanov@mail.ru>
2021-12-10 20:11:43 +0300

    Add base webhook view

--- tgbot/views.py
+++ tgbot/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import json
+import requests
 
-# Create your views here.
+from django.conf import settings
+from django.http import JsonResponse
+from django.views import View
+
+
+class TelegramBotWebhookView(View):
+
+    def post(self, request, *args, **kwargs):
+        t_data = json.loads(request.body)
+        chat_id = t_data["message"]["chat"]["id"]
+        TelegramBotWebhookView.send_message(chat_id, "Bye")
+        return JsonResponse({"ok": "POST request processed"})
+
+    @staticmethod
+    def send_message(chat_id, message):
+        url = f"{settings.TELEGRAM_URL}{settings.TELEGRAM_TOKEN}/sendMessage"
+        data = {
+            "chat_id": chat_id,
+            "text": message,
+            "parse_mode": "Markdown",
+        }
+        response = requests.post(url, data=data)
