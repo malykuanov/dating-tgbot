777a830875bfd441019cc4391433ea4625d60d2f
Mikhail Lykuanov <malykuanov@mail.ru>
2021-12-11 19:52:32 +0300

    Add user`s model

--- tgbot/models.py
+++ tgbot/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    chat_id = models.IntegerField(
+        verbose_name='Chat id пользователя'
+    )
+    first_name = models.CharField(
+        max_length=128,
+        verbose_name='Имя пользователя'
+    )
+    username = models.CharField(
+        max_length=64,
+        verbose_name='Ник пользователя'
+    )
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
