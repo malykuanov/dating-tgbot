2cc23bad32dfe69a31869966d3a3aea2952e8ff2
Mikhail Lykuanov <malykuanov@mail.ru>
2021-12-11 21:13:11 +0300

    Add webhook and move handlers to file

--- tgbot/views.py
+++ tgbot/views.py
@@ -1,25 +1,15 @@
-import json
-import requests
+from telebot import types
 
-from django.conf import settings
 from django.http import JsonResponse
 from django.views import View
+
+from .handlers import bot
 
 
 class TelegramBotWebhookView(View):
 
     def post(self, request, *args, **kwargs):
-        t_data = json.loads(request.body)
-        chat_id = t_data["message"]["chat"]["id"]
-        TelegramBotWebhookView.send_message(chat_id, "Bye")
+        json_str = request.body.decode('UTF-8')
+        update = types.Update.de_json(json_str)
+        bot.process_new_updates([update])
         return JsonResponse({"ok": "POST request processed"})
-
-    @staticmethod
-    def send_message(chat_id, message):
-        url = f"{settings.TELEGRAM_URL}{settings.TELEGRAM_TOKEN}/sendMessage"
-        data = {
-            "chat_id": chat_id,
-            "text": message,
-            "parse_mode": "Markdown",
-        }
-        response = requests.post(url, data=data)
