c8b98d8cfb4d6544b649c429812530e8e86388cb
Mikhail Lykuanov <malykuanov@mail.ru>
2021-12-15 20:41:44 +0300

    Add City model to admin panel

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import User, Profile
+from .models import City, Profile, User
 
 admin.site.register(User)
 admin.site.register(Profile)
+admin.site.register(City)
